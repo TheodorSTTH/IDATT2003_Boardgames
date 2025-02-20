e676343590f38a0510745c961ab3a97d5d41d076
TheodorSTTH <theodor.learn@gmail.com>
2025-02-20 13:32:41 +0100

    Added basic Hello, JavaFX sreen

--- src/main/java/edu/ntnu/irr/bidata/Main.java
+++ src/main/java/edu/ntnu/irr/bidata/Main.java
@@ -1,8 +1,25 @@
 package edu.ntnu.irr.bidata;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+    Label helloLabel = new Label("Hello, JavaFX!");
+    Scene scene = new Scene(helloLabel, 400, 300);
+
+    primaryStage.setTitle("My JavaFX App");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    launch(args);
   }
 }
