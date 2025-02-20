2d51e025634e1e7bab121f1712c9bc25d13f2909
TheodorSTTH <theodor.learn@gmail.com>
2025-02-20 12:16:21 +0100

    Create Die.java

---
+++ src/main/java/edu/ntnu/irr/bidata/Die.java
@@ -0,0 +1,26 @@
+package edu.ntnu.irr.bidata;
+import java.util.Random;
+
+/**
+ * Represents a single die with a given amount of faces.
+ * */
+public class Die {
+  private static final Random RANDOM = new Random();
+  private final int faces;
+
+  /**
+   * @param faces Amount of faces on die
+   * */
+  public Die(int faces) {
+    this.faces = faces;
+  }
+
+  /**
+   * Rolls the die and returns the result.
+   *
+   * @return random number between 1 and the amount of faces given
+   * */
+  public int roll() {
+    return RANDOM.nextInt(1, faces + 1);
+  }
+}
