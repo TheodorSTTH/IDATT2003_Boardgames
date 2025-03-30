8b224de183fe2ab2b86bba87e5d4d4b908a1121d
Imrerom05 <imre.romstad@gmail.com>
2025-03-30 11:52:00 +0200

    feat - added init to Risk

--- src/main/java/edu/ntnu/irr/bidata/Controler/Risk.java
+++ src/main/java/edu/ntnu/irr/bidata/Controler/Risk.java
@@ -1,10 +1,19 @@
 package edu.ntnu.irr.bidata.Controler;
+
 import edu.ntnu.irr.bidata.Model.Player;
+import edu.ntnu.irr.bidata.Model.BoardRisk;
 
 public class Risk extends Game {
+    private final BoardRisk board;
 
     public Risk(int amountOfPlayers) {
         super(amountOfPlayers);
+        this.board = new BoardRisk();
     }
 
+    @Override
+    protected void init() {
+        super.init();
+        //UI.toRiskGamePage();
+    }
 }
