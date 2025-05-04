d4c31cf06285f108d0d427025d021f2e98b6945a
Imrerom05 <imre.romstad@gmail.com>
2025-05-04 19:22:18 +0200

    feat: you can now chose how many tropes to place

--- src/main/java/edu/ntnu/irr/bidata/View/RiskGame/PlaceTroopsPane.java
+++ src/main/java/edu/ntnu/irr/bidata/View/RiskGame/PlaceTroopsPane.java
@@ -8,16 +8,22 @@ import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 
 public class PlaceTroopsPane extends AbstractSidebarPane {
   private Label avalibuleTropesTextField;
   private final ComboBox<Country> countryComboBox;
+  private final Spinner<Integer> amountOfTroopsSpinner;
   private final Button ok;
 
   public PlaceTroopsPane(Risk risk) {
     super(risk);
     this.setText("Place Troops");
     this.setLineSpacing(10);
+    this.amountOfTroopsSpinner = new Spinner<>(
+        new SpinnerValueFactory.IntegerSpinnerValueFactory(1, risk.getTroopsAvailable(), 1)
+    );
     this.countryComboBox = new ComboBox<>();
     this.ok = new Button("OK");
     avalibuleTropesTextField = new Label("You have " + Integer.toString(risk.getTroopsAvailable()) + " troops available");
@@ -30,15 +36,17 @@ public class PlaceTroopsPane extends AbstractSidebarPane {
     ok.setOnAction(event -> {
         boolean placingTroopsWasSuccessful = risk.placeTroops(
             countryComboBox.getValue().getName(), 
-            risk.getTroopsAvailable()
+            amountOfTroopsSpinner.getValue()
         );
-        if (placingTroopsWasSuccessful) notifyObservers();
-        else PopUp.showInfo("Something went wrong", "Adding troops on a square failed");
+      if (placingTroopsWasSuccessful)
+        notifyObservers();
+      else update();
     });
 
     getContainer().getChildren().addAll(
         new Label("Place troops on country"),
         countryComboBox,
+            amountOfTroopsSpinner,
         ok,
         avalibuleTropesTextField
     );
@@ -57,6 +65,8 @@ public class PlaceTroopsPane extends AbstractSidebarPane {
     avalibuleTropesTextField.setText("You have " + Integer.toString(risk.getTroopsAvailable()) + " troops available");
     countryComboBox.setItems(FXCollections.observableArrayList(placeOptions));
     countryComboBox.setValue(null);
+    ((SpinnerValueFactory.IntegerSpinnerValueFactory) amountOfTroopsSpinner.getValueFactory())
+        .setMax(risk.getTroopsAvailable());
     ok.setVisible(false);
   }
 
