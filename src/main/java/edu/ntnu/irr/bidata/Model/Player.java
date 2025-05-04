781726fb87732bf3d24d3636e0a0bd7dc2edac23
TheodorSTTH <theodor.learn@gmail.com>
2025-05-04 02:21:48 +0200

    Fix: Made saving work with updated player

--- src/main/java/edu/ntnu/irr/bidata/Model/Player.java
+++ src/main/java/edu/ntnu/irr/bidata/Model/Player.java
@@ -1,10 +1,17 @@
 package edu.ntnu.irr.bidata.Model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Player class i responsible for managing player piece/user.
  * */
 public class Player {
+
+  @JsonProperty
   private final String name;
+  @JsonProperty
   private final String color;
 
   public Player(String name, String color) {
@@ -22,5 +29,22 @@ public class Player {
   
   public String getColor() {
     return color;
+  }
+
+  /**
+   * Turns the player class into a key for json storage
+   * */
+  @JsonValue
+  public String toJsonKey() {
+    return name + ";" + color;
+  }
+
+  /**
+   * Reads json player key from file and turns it into player object.
+   * */
+  @JsonCreator
+  public static Player fromJsonKey(String key) {
+    String[] parts = key.split(";", 2);
+    return new Player(parts[0], parts.length>1 ? parts[1] : "");
   }
 }
