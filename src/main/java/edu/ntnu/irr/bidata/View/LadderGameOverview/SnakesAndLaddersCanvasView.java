477ed469333550b276fdb734795ed683e5fbb894
TheodorSTTH <theodor.learn@gmail.com>
2025-05-05 17:29:59 +0200

    docs: small bit of initial documentation

--- src/main/java/edu/ntnu/irr/bidata/View/LadderGameOverview/SnakesAndLaddersCanvasView.java
+++ src/main/java/edu/ntnu/irr/bidata/View/LadderGameOverview/SnakesAndLaddersCanvasView.java
@@ -17,6 +17,9 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 
+/**
+ * Is responsible for displaying board with events and players ready for the user to view.
+ * */
 public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<LaderGame> {
 
   private final int boardRowsAmount = 10;
@@ -26,6 +29,9 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
 
   private final ArrayList<CanvasTileView> tileViews = new ArrayList<>(boardColumnAmount * boardRowsAmount);
 
+  /**
+   * Constructs the board view variables.
+   * */
   public SnakesAndLaddersCanvasView(LaderGame snakesAndLadders) {
     for (int i = 0; i < 90; i++) {
       tileViews.add(null);
@@ -40,6 +46,10 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
     return tileViews.get(index);
   }
 
+  /**
+   * Renders the underlying board squares. Not including any events or players.
+   * Just tile underlying tile and number.
+   * */
   public void renderBoard(LaderGame snakesAndLadders) {
     GraphicsContext gc = getGraphicsContext2D();
     for (int row = 0; row < boardRowsAmount; row++) {
@@ -64,6 +74,9 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
     }
   }
 
+  /**
+   * Places players on all tile views without rendering them.
+   * */
   private void placePlayers(LaderGame snakesAndLadders) {
     HashMap<Player, Integer> playerPositions = snakesAndLadders.getPlayerPositions();
     for (Player player : playerPositions.keySet()) {
@@ -73,6 +86,9 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
     }
   }
 
+  /**
+   * Draws all players on board.
+   * */
   private void drawPlayers(LaderGame snakesAndLadders) {
     HashMap<Player, Integer> playerPositions = snakesAndLadders.getPlayerPositions();
     for (Player player : playerPositions.keySet()) {
@@ -82,6 +98,9 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
     }
   }
 
+  /**
+   * Draws all snakes and ladders on board.
+   * */
   private void drawSnakesAndLadders(LaderGame snakesAndLadders) {
     GraphicsContext gc = getGraphicsContext2D();
     HashMap<Integer, Event> events = snakesAndLadders.getBoard().getEvents();
@@ -92,6 +111,9 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
     });
   }
 
+  /**
+   * Draws all quiz boxes on board.
+   * */
   private void drawQuizEvents(LaderGame snakesAndLadders) {
     GraphicsContext gc = getGraphicsContext2D();
     HashMap<Integer, Event> events = snakesAndLadders.getBoard().getEvents();
@@ -127,16 +149,27 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
     GraphicsContext gc = getGraphicsContext2D();
     CanvasTileView fromTileView = tileViews.get(fromIndex);
     CanvasTileView toTileView = tileViews.get(toIndex);
+
+    // center position of from tile
     int fromCenterX = fromTileView.getCenterX();
     int fromCenterY = fromTileView.getCenterY();
+
+    // center position of to tile
     int toCenterX = toTileView.getCenterX();
     int toCenterY = toTileView.getCenterY();
+
+    // vector between from-to
     int dx = toCenterX - fromCenterX;
     int dy = toCenterY - fromCenterY;
-    double angle = Math.atan2(dy, dx);
+
+    double angle = Math.atan2(dy, dx); // vector angle
     double normal = angle - Math.PI / 2;
+
+    // normalized vector between from-to
     double normalVectorX = Math.cos(normal);
     double normalVectorY = Math.sin(normal);
+
+    // length of non-normalized vector
     double length = Math.sqrt(dx * dx + dy * dy);
 
     if (fromIndex < toIndex) { // We render a ladder
@@ -146,24 +179,34 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
 
       double offset = 10;
 
+      // left ladder pole point from
       double fromLeftX = fromCenterX - offset * normalVectorX;
       double fromLeftY = fromCenterY - offset * normalVectorY;
+
+      // right ladder pole point from
       double fromRightX = fromCenterX + offset * normalVectorX;
       double fromRightY = fromCenterY + offset * normalVectorY;
 
+      // left ladder pole point to
       double toLeftX = toCenterX - offset * normalVectorX;
       double toLeftY = toCenterY - offset * normalVectorY;
+
+      // right ladder pole point to
       double toRightX = toCenterX + offset * normalVectorX;
       double toRightY = toCenterY + offset * normalVectorY;
 
       double distanceBetweenLadderSteps = 20;
       int amountOfSteps = (int) Math.floor(length / distanceBetweenLadderSteps);
+
+      // draws all steps on the ladder
       for (int i = 1; i < amountOfSteps; i++) {
         double t = i / (double) amountOfSteps; // curve param in range 0 to 1
 
+        // left point from
         double stepLeftX = fromLeftX + t * dx;
         double stepLeftY = fromLeftY + t * dy;
 
+        // right point from
         double stepRightX = fromRightX + t * dx;
         double stepRightY = fromRightY + t * dy;
 
@@ -177,7 +220,7 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
       gc.setLineWidth(5.0);
       gc.setLineCap(StrokeLineCap.ROUND);
 
-      int amountOfSteps = 300;
+      int amountOfSteps = 300; // TODO: Scale with length & amplitude
       double amplitude = 10;
 
       gc.beginPath();
@@ -185,13 +228,16 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
 
       for (int i = 1; i < amountOfSteps; i++) {
         double t = i / (double) amountOfSteps; // curve param in range 0 to 1
-
         double distanceTraveled = t * length;
         double pixelsPerWiggle = 60;
+
+        // current angle of snake
         double curveAngle = (distanceTraveled / pixelsPerWiggle) * (2 * Math.PI);
-        double offset = amplitude * Math.sin(curveAngle);
+        double offset = amplitude * Math.sin(curveAngle); // from center
+
         double perpX = -dy / length;
         double perpY = dx / length;
+
         double x = fromCenterX + dx * t + offset * perpX;
         double y = fromCenterY + dy * t + offset * perpY;
 
@@ -203,16 +249,14 @@ public class SnakesAndLaddersCanvasView extends Canvas implements IObserver<Lade
   }
 
   /**
-   * Re-renders the current board and places players on the board. Effectively updating
-   * the board for the user.
+   * Renders the current board with quiz boxes, snakes and ladders. Also places & renders players
+   * on the board. Effectively updating the board for the user.
    *
    * @param snakesAndLadders is the current snakes and ladders game object.
    * */
   @Override
   public void update(LaderGame snakesAndLadders) {
     renderBoard(snakesAndLadders);
-    //renderSnakes(snakesAndLadders);
-    //renderLadders(snakesAndLadders);
     drawQuizEvents(snakesAndLadders);
     drawSnakesAndLadders(snakesAndLadders);
     placePlayers(snakesAndLadders);
