56ab1d8f9a8fe8c8577a63a0006bbb7151a612d7
Imrerom05 <imre.romstad@gmail.com>
2025-05-07 13:33:05 +0200

    feat: added exit game buton to risk

--- src/main/java/edu/ntnu/irr/bidata/View/RiskGame/RiskPage.java
+++ src/main/java/edu/ntnu/irr/bidata/View/RiskGame/RiskPage.java
@@ -1,9 +1,10 @@
 package edu.ntnu.irr.bidata.View.RiskGame;
 
+import edu.ntnu.irr.bidata.Controler.NavigationManager;
 import edu.ntnu.irr.bidata.Model.Risk.Country;
 import edu.ntnu.irr.bidata.Model.Risk.Risk;
 import edu.ntnu.irr.bidata.View.PopUp;
-
+import edu.ntnu.irr.bidata.View.StartPage.StartPage;
 import javafx.scene.control.Label;
 import java.util.HashMap;
 
@@ -35,10 +36,16 @@ public class RiskPage extends HBox {
     Button saveButton = new Button("SAVE");
     saveButton.getStyleClass().add("fantasy-button");
 
+    Button exitGameButton = new Button("Exit game");
+    exitGameButton.getStyleClass().addAll("fantasy-button");
+    exitGameButton.setOnAction(e -> {
+      NavigationManager.navigate(new StartPage());
+      });
 
-    HBox underlay = new HBox(bonusesLabel, saveButton);
+
+    HBox underlay = new HBox(bonusesLabel, saveButton, exitGameButton);
     underlay.setAlignment(Pos.CENTER);
-    underlay.setSpacing(20);
+    underlay.setSpacing(15);
 
 
     VBox mainStage = new VBox(10);
