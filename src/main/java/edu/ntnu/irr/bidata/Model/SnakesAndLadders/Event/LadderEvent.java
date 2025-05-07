1a61923e503e117eea0a0605ce8ab81009d98878
Imrerom05 <imre.romstad@gmail.com>
2025-05-07 15:38:04 +0200

    docs: added java doc and to LadderEvent and QuizEvent

--- src/main/java/edu/ntnu/irr/bidata/Model/SnakesAndLadders/Event/LadderEvent.java
+++ src/main/java/edu/ntnu/irr/bidata/Model/SnakesAndLadders/Event/LadderEvent.java
@@ -1,26 +1,56 @@
 package edu.ntnu.irr.bidata.Model.SnakesAndLadders.Event;
 
+/**
+ * Represents a ladder event in the Snakes and Ladders game.
+ * <p>
+ * When triggered, this event moves the player to a new destination square.
+ */
 public class LadderEvent extends Event {
-  private int destination;
+    private int destination; // The square number the player moves to when landing on a ladder
 
-  public LadderEvent() {
-      // For Json Saving and Loading
-  }
+    /**
+     * Default no-argument constructor.
+     * <p>
+     * Required for JSON serialization/deserialization frameworks like Jackson.
+     */
+    public LadderEvent() {
+        // No initialization needed here
+    }
 
-  public LadderEvent(int destination) {
-      this.destination = destination;
-  }
+    /**
+     * Constructs a LadderEvent with a specific destination square.
+     *
+     * @param destination the board square the player will move to
+     */
+    public LadderEvent(int destination) {
+        this.destination = destination;
+    }
 
-  @Override
-  public int action() {
-      return destination;
-  }
+    /**
+     * Executes the ladder event logic by returning the destination square.
+     *
+     * @return the destination square number
+     */
+    @Override
+    public int action() {
+        return destination;
+    }
 
-  public int getDestination() {
-      return destination;
-  }
+    /**
+     * Gets the destination square.
+     *
+     * @return the destination square number
+     */
+    public int getDestination() {
+        return destination;
+    }
 
-  public void setDestination(int destination) {
-      this.destination = destination;
-  }
+    /**
+     * Sets the destination square.
+     *
+     * @param destination the new destination square number
+     */
+    public void setDestination(int destination) {
+        this.destination = destination;
+    }
 }
